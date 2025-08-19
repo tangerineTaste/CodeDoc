9a4c6b453abb765fadc9996583a15ca7ae41218c
tangerineTaste <sim7178@naver.com>
2025-08-19 15:44:19 +0900

    gitignore 수정

--- 4.웹구현/codedoc_web/chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "chatbot"
-urlpatterns = [
-    path("", views.chatbot_view, name="chatbot_view"),
-    path("api/", views.chatbot_api, name="chatbot_api"),
-]
